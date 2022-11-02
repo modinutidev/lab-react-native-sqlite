8716dfc4d3bf3b682777510f127424a12ce74394
Bruno Modinuti <modinutidev@gmail.com>
2022-11-02 11:06:30 -0300

    Criado funções para o CRUD

--- src/services/Notas.js
+++ src/services/Notas.js
@@ -19,7 +19,7 @@ export async function adicionaNota(nota) {
         "INSERT INTO Notas (titulo, categoria, texto) VALUES (?, ?, ?);",
         [titulo, categoria, texto],
         () => {
-          resolve("Nota adicionada");
+          resolve("Nota adicionada!");
         }
       );
     });
@@ -36,6 +36,46 @@ export async function buscarNotas() {
           resolve(result.rows._array);
         }
       );
+    });
+  });
+}
+
+export async function buscarNota(idNota) {
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql(
+        "SELECT * FROM Notas WHERE id=?;",
+        [idNota],
+        (transaction, result) => {
+          resolve(result.rows._array);
+        }
+      );
+    });
+  });
+}
+
+export async function editaNota(nota) {
+  const { titulo, categoria, texto, id } = nota;
+
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql(
+        "UPDATE Notas SET titulo=?, categoria=?, texto=? WHERE id=?;",
+        [titulo, categoria, texto, id],
+        () => {
+          resolve("Nota atualizada com sucesso!");
+        }
+      );
+    });
+  });
+}
+
+export async function deletarNota(idNota) {
+  return new Promise((resolve) => {
+    db.transaction((transaction) => {
+      transaction.executeSql("DELETE FROM Notas WHERE id=?;", [idNota], () => {
+        resolve("Nota deletada!");
+      });
     });
   });
 }
